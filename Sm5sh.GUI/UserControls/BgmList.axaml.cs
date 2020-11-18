d15d92a275aba68f6b89bf0f915e2b591a3d16df
deihnyx <deihnyx@gmail.com>
2020-11-18 00:38:18 -0600

    wip

---
+++ Sm5sh.GUI/UserControls/BgmList.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Sm5sh.GUI.UserControls
+{
+    public class BgmList : UserControl
+    {
+        public BgmList()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
