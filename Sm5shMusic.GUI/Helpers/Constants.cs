8fbbf9646c3b144ab58e9d449b1dc298c250681f
deihnyx <deihnyx@gmail.com>
2020-12-17 21:53:02 -0600

    update versions

--- Sm5shMusic.GUI/Helpers/Constants.cs
+++ Sm5shMusic.GUI/Helpers/Constants.cs
@@ -4,7 +4,7 @@ namespace Sm5shMusic.GUI.Helpers
 {
     public static class Constants
     {
-        public const double GUIVersion = 0.6;
+        public const double GUIVersion = 0.7;
 
         public class SpecialCategories
         {
