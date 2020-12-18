a30e5607d334915f33aabc4a371c9a0a0d860411
deihnyx <deihnyx@gmail.com>
2020-12-17 21:53:20 -0600

    update versions

--- Sm5sh.CLI/Script.cs
+++ Sm5sh.CLI/Script.cs
@@ -13,7 +13,7 @@ namespace Sm5sh.CLI
         private readonly IStateManager _state;
         private readonly IServiceProvider _serviceProvider;
         private readonly IWorkspaceManager _workspace;
-        private const double CLIVersion = 0.5;
+        private const double CLIVersion = 0.7;
 
         public Script(IServiceProvider serviceProvider, IWorkspaceManager workspace, IStateManager state, ILogger<Script> logger)
         {
